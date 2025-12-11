8f35859a4e334e0cc7b72f6eb2c6dfc6f9074cd6
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2025-12-11 17:33:25 +0100

    fix: no sonar

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@
  */
 
 // 1. Import the class from the source
+// This allows import Core from 'mdns-listener-advanced'
+
 import { Core } from "./Core.js";
 
 // 2. Re-export shared types
 export * from "./types.js";
 
 // 3. Export as Named Export
-export { Core };
+export { Core }; // NOSONAR annoying sonarcloud issue with export default
 
 // 4. Export as Default Export
-export default Core;
+// This allows both import Core from 'mdns-listener-advanced' and import { Core } from 'mdns-listener-advanced'
+export default Core; // NOSONAR annoying sonarcloud issue with export default
