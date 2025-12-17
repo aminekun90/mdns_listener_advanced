0babe4783b7c632a0f26ded4a887745f903c4d46
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2025-12-17 11:46:35 +0100

    fix: cleanup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
-    dts: true,
-    clean: true,
-    sourcemap: true,
-    outDir: 'dist',
-    target: 'es2022',
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  outDir: "dist",
+  target: "es2022",
 });
