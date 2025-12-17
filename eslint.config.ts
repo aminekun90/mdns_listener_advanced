f492a86bc5a4a6b779ca759234080fa867881786
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2025-12-17 17:13:02 +0100

    chore: tsconfig for js config files

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,11 @@
+// eslint.config.js
 import js from "@eslint/js";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 import vitest from "eslint-plugin-vitest";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+
 export default defineConfig([
   {
     ignores: ["node_modules/**", "dist/**", "build/**", "coverage/**"],
@@ -12,12 +14,10 @@ export default defineConfig([
   js.configs.recommended,
   ...tseslint.configs.recommended,
 
-  // Prettier (First, to set baseline)
   prettierRecommended,
 
   {
     files: ["src/**/*.{ts,js}", "__tests__/**/*.{ts,js}"],
-
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -25,21 +25,28 @@ export default defineConfig([
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
-
       globals: {
         ...globals.node,
         ...globals.es2021,
       },
     },
-
     rules: {
       "no-console": "warn",
-
       "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
-
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+  },
+
+  // ✅ Node CommonJS config files
+  {
+    files: ["commitlint.config.js", "*.config.js", "*.config.cjs"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
     },
   },
 
