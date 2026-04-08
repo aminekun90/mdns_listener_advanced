05aa4b6468582bbfafea3847a6bfc2495061c216
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2026-04-08 11:49:07 +0200

    chore: update deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["./src/index.ts"],
   format: ["esm", "cjs"],
-  dts: true,
+  dts: false, // <--- SHUT THIS OFF to stop the tsup crash
   clean: true,
-  sourcemap: true,
+  sourcemap: false,
   outDir: "dist",
   target: "es2022",
+  bundle: true,
+  external: ["multicast-dns"],
 });
